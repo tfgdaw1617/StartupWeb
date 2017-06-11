23d25007496a2d4fb082d1a2b00c4327ae429c32
Gerard <Gerard@172.16.206.201>
2017-06-11 02:41:39 +0200

    Proyecto semi finalizado

--- StartupWeb/bin/static/js/userIndex.js
+++ StartupWeb/bin/static/js/userIndex.js
@@ -1,32 +1,39 @@
-$(function(){
-	$('.count').each(function () {
-	    $(this).prop('Counter',0).animate({
-	        Counter: $(this).text()
-	    }, {
-	        duration: 2000,
-	        easing: 'swing',
-	        step: function (now) {
-	            $(this).text(Math.ceil(now));
-	        }
-	    });
+$(function() {
+	var $ppc = $('.progress-pie-chart'), percent = parseInt($ppc
+			.data('percent')), deg = 360 * percent / 100;
+	if (percent > 50) {
+		$ppc.addClass('gt-50');
+	}
+	$('.ppc-progress-fill').css('transform', 'rotate(' + deg + 'deg)');
+	$('.ppc-percents span').html(percent + '%');
+	
+	$('.count').each(function() {
+		$(this).prop('Counter', 0).animate({
+			Counter : $(this).text()
+		}, {
+			duration : 20,
+			easing : 'swing',
+			step : function(now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
 	});
-	
-	
-	jQuery('.skillbar').each(function(){
+
+	jQuery('.skillbar').each(function() {
 		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},2000);
+			width : jQuery(this).attr('data-percent')
+		}, 2000);
 	});
-	
+
 	$("#imgPerfil").on("mouseenter", function() {
-	      $("#popupEditaImagen").show();
+		$("#popupEditaImagen").show();
 	}).on("mouseleave", function() {
-	      $("#popupEditaImagen").hide();
+		$("#popupEditaImagen").hide();
 	});
 	$("#popupEditaImagen").on("mouseenter", function() {
-	      $("#popupEditaImagen").show();
+		$("#popupEditaImagen").show();
 	}).on("mouseleave", function() {
-	      $("#popupEditaImagen").hide();
+		$("#popupEditaImagen").hide();
 	});
-	
+
 });
